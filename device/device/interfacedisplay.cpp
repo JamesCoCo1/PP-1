eeb432401ca3f1f19c82c78ab4620e9492b7c98f
JiangxuanLi <jonathan1214@foxmail.com>
2020-07-28 11:33:17 +0800

    给出一个简单的layou布局模板

--- device/device/interfacedisplay.cpp
+++ device/device/interfacedisplay.cpp
@@ -12,14 +12,29 @@ InterfaceDisplay::InterfaceDisplay(int maxE, int maxS, int maxI, QWidget *parent
     this->ibp2Drawer = new QWidgetDrawForSerialRx(this->maxIBP2);
     this->spo2Drawer = new QWidgetDrawForSerialRx(this->maxSPO2);
 
-    this->initLayout();
+    this->initMainLayout();
 
     // 这个只是测试用的 还需要添加更多的控件
     this->resize(600, 400);
-    this->setLayout(drawerLayout);
+    this->setLayout(this->mainLayout);
 }
 
-void InterfaceDisplay::initLayout()
+void InterfaceDisplay::initMainLayout()
+{
+    this->initDrawerLayout();
+    this->initRightSideWidgetLayout();
+
+    // 初始化主 layout
+    this->mainLayout = new QHBoxLayout();
+    this->mainLayout->addLayout(this->drawerLayout);
+    this->mainLayout->addLayout(this->rightSideWidgetLayout);
+    // 配置拉伸因子
+    this->mainLayout->setStretchFactor(this->drawerLayout, 2);
+    this->mainLayout->setStretchFactor(this->rightSideWidgetLayout, 1);
+}
+
+// 初始化画图部分的 layout
+void InterfaceDisplay::initDrawerLayout()
 {
     this->ecgDrawer->setSizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
     this->ibp2Drawer->setSizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
@@ -35,4 +50,27 @@ void InterfaceDisplay::initLayout()
     this->drawerLayout->addWidget(this->ecgDrawer);
     this->drawerLayout->addWidget(this->spo2Drawer);
     this->drawerLayout->addWidget(this->ibp2Drawer);
+}
+
+void InterfaceDisplay::initRightSideWidgetLayout()
+{
+    this->rightSideWidgetLayout = new QVBoxLayout();
+
+    this->widgetHr = new QLabel();
+    this->widgetNibp = new QLabel();
+    widgetHr->setText("HR");
+    widgetNibp->setText("NiBP");
+
+    this->btnUp = new QPushButton("UP");
+    this->btnDowm = new QPushButton("DOWN");
+
+    this->btnUp->setSizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
+    this->btnDowm->setSizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
+
+    // 加入 layout
+    this->rightSideWidgetLayout->addWidget(widgetHr);
+    this->rightSideWidgetLayout->addWidget(widgetNibp);
+    this->rightSideWidgetLayout->addWidget(btnUp);
+    this->rightSideWidgetLayout->addWidget(btnDowm);
+
 }
